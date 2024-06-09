c39f3cde286ed0c734596749ef6ba01c7151839e
Mahmood-TF <mahmood.tgv5320@gmail.com>
2024-06-09 14:08:55 +0330

    Perform crossover operations to create new candidate solutions

--- Sudoku_solver.py
+++ Sudoku_solver.py
@@ -262,6 +262,103 @@ class Tournament(object):
         if (r < selection_rate):
             return fittest
         else:
-            return weakest        
+            return weakest
+        
+
+class CycleCrossover(object):
+    """ Crossover relates to the analogy of genes within each parent candidate
+    mixing together in the hopes of creating a fitter child candidate."""
+
+    def crossover(self, parent1, parent2, crossover_rate):
+        """ Create two new child candidates by crossing over parent genes. """
+        child1 = Candidate()
+        child2 = Candidate()
+
+        # Make a copy of the parent genes.
+        child1.values = np.copy(parent1.values)
+        child2.values = np.copy(parent2.values)
+
+        r = random.uniform(0, 1.1)
+        while (r > 1):  # Outside [0, 1] boundary. Choose another.
+            r = random.uniform(0, 1.1)
+
+        # Perform crossover.
+        if (r < crossover_rate):
+            # Pick a crossover point. Crossover must have at least 1 row (and at most Nd-1) rows.
+            crossover_point1 = random.randint(0, 8)
+            crossover_point2 = random.randint(1, 9)
+            while (crossover_point1 == crossover_point2):
+                crossover_point1 = random.randint(0, 8)
+                crossover_point2 = random.randint(1, 9)
+
+            if (crossover_point1 > crossover_point2):
+                temp = crossover_point1
+                crossover_point1 = crossover_point2
+                crossover_point2 = temp
+
+            for i in range(crossover_point1, crossover_point2):
+                child1.values[i], child2.values[i] = self.crossover_rows(child1.values[i], child2.values[i])
+
+        return child1, child2
+
+    def crossover_rows(self, row1, row2):
+        child_row1 = np.zeros(Nd)
+        child_row2 = np.zeros(Nd)
+
+        remaining = list(range(1, Nd + 1))
+        cycle = 0
+
+        while ((0 in child_row1) and (0 in child_row2)):  # While child rows not complete...
+            if (cycle % 2 == 0):  # Even cycles.
+                # Assign next unused value.
+                index = self.find_unused(row1, remaining)
+                start = row1[index]
+                if start in remaining:
+                    remaining.remove(start)
+                child_row1[index] = row1[index]
+                child_row2[index] = row2[index]
+                next = row2[index]
+
+                while (next != start):  # While cycle not done...
+                    index = self.find_value(row1, next)
+                    if row1[index] in remaining:
+                        remaining.remove(row1[index])
+                    child_row1[index] = row1[index]
+                    child_row2[index] = row2[index]
+                    next = row2[index]
+
+                cycle += 1
+
+            else:  # Odd cycle - flip values.
+                index = self.find_unused(row1, remaining)
+                start = row1[index]
+                if start in remaining:
+                    remaining.remove(start)
+                child_row1[index] = row2[index]
+                child_row2[index] = row1[index]
+                next = row2[index]
+
+                while (next != start):  # While cycle not done...
+                    index = self.find_value(row1, next)
+                    if row1[index] in remaining:
+                        remaining.remove(row1[index])
+                    child_row1[index] = row2[index]
+                    child_row2[index] = row1[index]
+                    next = row2[index]
+
+                cycle += 1
+
+        return child_row1, child_row2
+
+    def find_unused(self, parent_row, remaining):
+        for i in range(0, len(parent_row)):
+            if (parent_row[i] in remaining):
+                return i
+
+    def find_value(self, parent_row, value):
+        for i in range(0, len(parent_row)):
+            if (parent_row[i] == value):
+                return i
+
         
     
