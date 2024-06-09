7526d7d7010bc46ea041ff48b7d69c94e3bc3941
Mahmood-TF <mahmood.tgv5320@gmail.com>
2024-06-09 10:59:55 +0330

    Initialize Population class: Create a population of candidate solutions.

---
+++ Sudoku_solver.py
@@ -0,0 +1,78 @@
+import numpy as np
+import random
+import operator
+
+random.seed()
+
+Nd = 9  
+
+class Population(object):
+    """ A set of candidate solutions to the Sudoku puzzle.
+    These candidates are also known as the chromosomes in the population. """
+
+    def __init__(self):
+        self.candidates = []
+
+
+    def seed(self, Nc, given):
+        self.candidates = []
+
+        # Determine the legal values that each square can take.
+        helper = Candidate()
+        helper.values = [[[] for j in range(0, Nd)] for i in range(0, Nd)]
+        for row in range(0, Nd):
+            for column in range(0, Nd):
+                for value in range(1, 10):
+                    if ((given.values[row][column] == 0) and not (given.is_column_duplicate(column, value) or given.is_block_duplicate(row, column, value) or given.is_row_duplicate(row, value))):
+                        # Value is available.
+                        helper.values[row][column].append(value)
+                    elif given.values[row][column] != 0:
+                        # Given/known value from file.
+                        helper.values[row][column].append(given.values[row][column])
+                        break
+
+        # Seed a new population.
+        for p in range(0, Nc):
+            g = Candidate()
+            for i in range(0, Nd):  # New row in candidate.
+                row = np.zeros(Nd)
+
+                # Fill in the givens.
+                for j in range(0, Nd):  # New column j value in row i.
+
+                    # If value is already given, don't change it.
+                    if given.values[i][j] != 0:
+                        row[j] = given.values[i][j]
+                    # Fill in the gaps using the helper board.
+                    elif given.values[i][j] == 0:
+                        row[j] = helper.values[i][j][random.randint(0, len(helper.values[i][j]) - 1)]
+
+                # There must be no duplicates in the row.
+                ii = 0
+                while len(list(set(row))) != Nd:
+                    ii += 1
+                    if ii > 500000:
+                        return 0
+                    for j in range(0, Nd):
+                        if given.values[i][j] == 0:
+                            row[j] = helper.values[i][j][random.randint(0, len(helper.values[i][j]) - 1)]
+
+                g.values[i] = row
+            self.candidates.append(g)
+        # Compute the fitness of all candidates in the population.
+        self.update_fitness()
+
+
+        return 1
+
+    def update_fitness(self):
+        """ Update fitness of every candidate/chromosome. """
+        for candidate in self.candidates:
+
+            candidate.update_fitness()
+    def sort(self):
+        """ Sort the population based on fitness. """
+        self.candidates = sorted(self.candidates, key=operator.attrgetter('fitness'))
+
+
+
