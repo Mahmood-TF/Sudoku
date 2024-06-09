72ac4c437a26a4febddebdad72c7ab2f8ad300d3
Mahmood-TF <mahmood.tgv5320@gmail.com>
2024-06-09 12:09:32 +0330

    Introduce mutation to a candidate solution based on a mutation rate

--- Sudoku_solver.py
+++ Sudoku_solver.py
@@ -135,3 +135,36 @@ class Candidate(object):
 
         self.fitness = fitness
 
+
+
+    def mutate(self, mutation_rate, given):
+        """ Mutate a candidate by picking a row, and then picking two values within that row to swap. """
+
+        r = random.uniform(0, 1.1)
+        while r > 1:  # Outside [0, 1] boundary - choose another
+            r = random.uniform(0, 1.1)
+
+        success = False
+        if r < mutation_rate:  # Mutate.
+            while not success:
+                row1 = random.randint(0, 8)
+                row2 = random.randint(0, 8)
+                row2 = row1
+
+                from_column = random.randint(0, 8)
+                to_column = random.randint(0, 8)
+                while from_column == to_column:
+                    from_column = random.randint(0, 8)
+                    to_column = random.randint(0, 8)
+
+                    # Check if the two places are free to swap
+                if given.values[row1][from_column] == 0 and given.values[row1][to_column] == 0:
+                    # ...and that we are not causing a duplicate in the rows' columns.
+                    if not given.is_column_duplicate(to_column, self.values[row1][from_column]) and not given.is_column_duplicate(from_column, self.values[row2][to_column]) and not given.is_block_duplicate(row2, to_column, self.values[row1][from_column]) and not given.is_block_duplicate(row1, from_column, self.values[row2][to_column]):
+                        # Swap values.
+                        temp = self.values[row2][to_column]
+                        self.values[row2][to_column] = self.values[row1][from_column]
+                        self.values[row1][from_column] = temp
+                        success = True
+
+        return success
