b921a55c574fc97d4eb08a03a2fa7070f2f686fa
Mahmood-TF <mahmood.tgv5320@gmail.com>
2024-06-09 13:17:40 +0330

    Add methods to check for duplicates blocks

--- Sudoku_solver.py
+++ Sudoku_solver.py
@@ -189,3 +189,24 @@ class Fixed(Candidate):
             if self.values[row][column] == value:
                 return True
         return False
+    
+    def is_block_duplicate(self, row, column, value):
+        """ Check duplicate in a 3 x 3 block. """
+        i = 3 * (int(row / 3))
+        j = 3 * (int(column / 3))
+
+        if ((self.values[i][j] == value)
+            or (self.values[i][j + 1] == value)
+            or (self.values[i][j + 2] == value)
+            or (self.values[i + 1][j] == value)
+            or (self.values[i + 1][j + 1] == value)
+            or (self.values[i + 1][j + 2] == value)
+            or (self.values[i + 2][j] == value)
+            or (self.values[i + 2][j + 1] == value)
+            or (self.values[i + 2][j + 2] == value)):
+            return True
+        else:
+            return False
+        
+        
+    
