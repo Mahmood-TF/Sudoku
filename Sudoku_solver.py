e65c859fa2ac7092861558c7e42319b44c8ab1ae
Mahmood-TF <mahmood.tgv5320@gmail.com>
2024-06-09 14:04:31 +0330

    Initialize Tournament class: Select two parents from the population for crossover

--- Sudoku_solver.py
+++ Sudoku_solver.py
@@ -233,6 +233,35 @@ class Fixed(Candidate):
                     if cnt1 > 1 or cnt2 > 1 or cnt3 > 1:
                         return False
         return True
-        
+    
+    
+
+class Tournament(object):
+    """ The crossover function requires two parents to be selected from the population pool. The Tournament class is used to do this."""
+
+    def compete(self, candidates):
+        """ Pick 2 random candidates from the population and get them to compete against each other. """
+        c1 = candidates[random.randint(0, len(candidates) - 1)]
+        c2 = candidates[random.randint(0, len(candidates) - 1)]
+        f1 = c1.fitness
+        f2 = c2.fitness
+
+        # Find the fittest and the weakest.
+        if (f1 > f2):
+            fittest = c1
+            weakest = c2
+        else:
+            fittest = c2
+            weakest = c1
+
+        # selection_rate = 0.85
+        selection_rate = 0.80
+        r = random.uniform(0, 1.1)
+        while (r > 1):  # Outside [0, 1] boundary. Choose another.
+            r = random.uniform(0, 1.1)
+        if (r < selection_rate):
+            return fittest
+        else:
+            return weakest        
         
     
